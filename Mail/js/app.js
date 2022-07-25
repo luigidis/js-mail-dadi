b7f99c31e9cc5b14141c72c51d55ef75cc637356
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-25 16:30:54 +0200

    Esercizio Email funzionante

--- Mail/js/app.js
+++ Mail/js/app.js
@@ -1,3 +1,18 @@
 // console.log('ciao')
 
-prompt('Inserisci la tua email')
+const emailUser = prompt('Inserisci la tua email');
+
+// creo un array dove consservo tutte le email che ho
+const allEmails = ['pippo@prova.com', 'tizio@prova.com', 'sara@prova.com', 'grazia@prova.com'];
+// console.log(allEmails);
+// console.log(allEmails.length);
+
+// Per ogni "cassetto" dell'array adesso dovrò verificare se ho il numero
+
+for (let i = 0; i < allEmails.length; i++) {
+    // console.log(i);
+    // console.log(allEmails)
+    if (emailUser === allEmails[i]) {
+        alert('email consentita')
+    }
+}
