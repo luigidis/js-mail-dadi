95210a4cda2df1fe7553f6c33bc0920764a83593
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-25 17:00:54 +0200

    Esercizio Dadi completato

--- Gioco dei dadi/js/app.js
+++ Gioco dei dadi/js/app.js
@@ -1,1 +1,20 @@
-console.log('ciao')
+// console.log('ciao');
+
+//Mi creo una variabile dove metterò il primo dado e lo "lancio"
+const primoDado = ((Math.floor(Math.random() * 6)) + 1);
+console.log(primoDado);
+// Mi creo una variabile dove metterò il secondo dado e lo "lancio"
+const secondoDado = ((Math.floor(Math.random() * 6)) +  1)
+console.log(secondoDado)
+
+// Se Primo Dado > Secondo Dado
+if (primoDado > secondoDado) {
+    console.log('Vince giocatore 1')
+} else if (primoDado < secondoDado) {
+    // Se Primo Dado < Secondo Dado
+    console.log('Vince giocatore 2')
+} else {
+    // Se Primo Dado = Secondo Dado
+    console.log('Risultato pari, tirare i dadi di nuovo')
+}
+
